b0841ab03d381a4bb2279cfc293fe7f6f6826622
luciano <luciano.fndz@gmail.com>
2020-11-15 18:26:16 -0300

    004 - added stars

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from 'react';
+import ReactStars from "react-rating-stars-component";
 import { getProductById } from '../api/productsApi';
 import '../styles/ProductStyles.css';
 import { Button, Toast } from 'react-bootstrap';
@@ -7,7 +8,8 @@ import { useParams } from 'react-router-dom';
 
 export function ProductView(props) {
   const [show, setShow] = useState(false);
-  const [product, setProduct] = useState(false);
+  const [product, setProduct] = useState(null);
+  const [stars, setStars] = useState(0);
   const { id } = useParams();
 
   useEffect(() => {
@@ -21,10 +23,28 @@ export function ProductView(props) {
       const response = await getProductById(parseInt(id));
       if (response) {
         setProduct(response);
+        setStarsAverage(response.RatingAvg);
       }
     }
   };
 
+  const setStarsAverage = (number) => {
+    if (number) {
+      let roundedNumber = 0;
+      
+      if ((number % 1) >= 0.5) {
+        roundedNumber = parseFloat(parseInt(number) + 0.5);            
+        debugger;
+      } else {
+        roundedNumber = parseInt(number);
+        debugger;
+      }
+      setStars(roundedNumber);
+      debugger;
+    }
+  };
+  console.log("stars", stars);
+                                                                                                 
   return (
     <div className="product-main-container">
       <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
@@ -39,38 +59,48 @@ export function ProductView(props) {
         </Toast.Header>
         <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
       </Toast>
-      <div className="product">
-        <div>
+      {product && 
+        <div className="product">
           <div>
-            <img src={product.PictureURL ? product.PictureURL : ''} alt="product"></img>
-            <div className="image-buttons-container">
-              <Button variant="danger">Clearance</Button>
-              <Button variant="danger">Free Shipping</Button>
+            <div>
+              <img src={product.PictureURL ? product.PictureURL : ''} alt="product"></img>
+              <div className="image-buttons-container">
+                <Button variant="danger">Clearance</Button>
+                <Button variant="danger">Free Shipping</Button>
+              </div>
+            </div>
+            <div className="product-image__footer-text">
+              <p><b>Brand: </b>{product.Brand ? product.Brand : ''}</p>
+              <p><b>Color: </b>{product.Color ? product.Color : ''}</p>
             </div>
           </div>
-          <div className="product-image__footer-text">
-            <p><b>Brand: </b>{product.Brand ? product.Brand : ''}</p>
-            <p><b>Color: </b>{product.Color ? product.Color : ''}</p>
-          </div>
-        </div>
-        <div>
-          <h3>{product.Name ? product.Name : ''}</h3>
-          <p>{product.Description ? product.Description : ''}</p>
-        </div>
-       
-        <div>
           <div>
+            <h3>{product.Name ? product.Name : ''}</h3>
+            <ReactStars
+              count={5}
+              size={24}
+              activeColor="#ffd700"
+              isHalf={true}
+              value={stars}
+              edit={false}
+            />
+            <p>{product.Description ? product.Description : ''}</p>
+          </div>
+        
+          <div>
+            <div>
 
-          </div>
-          <div>
-            {product.Stock 
-              ? <p>Available {product.Stock} units</p>
-              : <p>Out of Stock</p>
-            }
-            <Button onClick={() => setShow(true)}>Show Toast</Button>
+            </div>
+            <div>
+              {product.Stock 
+                ? <p>Available {product.Stock} units</p>
+                : <p>Out of Stock</p>
+              }
+              <Button onClick={() => setShow(true)}>Show Toast</Button>
+            </div>
           </div>
         </div>
-      </div>
+      }
     </div>
   ); 
 }
